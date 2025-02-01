08f2d5f6bff6e4f496ce033b1e237a6c382b71e2
For8stt <xkisil@stuba.sk>
2025-02-01 23:31:26 +0100

    logging

--- src/main/java/com/example/demo/exeptions/ProductNotFoundExeption.java
+++ src/main/java/com/example/demo/exeptions/ProductNotFoundExeption.java
@@ -1,11 +1,17 @@
 package com.example.demo.exeptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductNotFoundExeption extends RuntimeException {
+
+    private final static Logger logger = LoggerFactory.getLogger(ProductNotFoundExeption.class);
+
     public ProductNotFoundExeption(){
         super(ErrorMessages.PRODUCT_NOT_FIND.getMessage());
+        logger.error("Exception: "+getClass()+"throw");
     }
 }
--- src/main/java/com/example/demo/product/services/GetProductService.java
+++ src/main/java/com/example/demo/product/services/GetProductService.java
@@ -5,6 +5,8 @@ import com.example.demo.exeptions.ProductNotFoundExeption;
 import com.example.demo.product.ProductRepository;
 import com.example.demo.product.model.Product;
 import com.example.demo.product.model.ProductDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ import java.util.Optional;
 public class GetProductService implements Query<Integer, ProductDTO> {
 
     private final ProductRepository productRepository;
+    private final static Logger logger=  LoggerFactory.getLogger(GetProductService.class);
 
     public GetProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
@@ -21,6 +24,7 @@ public class GetProductService implements Query<Integer, ProductDTO> {
 
     @Override
     public ResponseEntity<ProductDTO> execute(Integer input) {
+        logger.info("Executing "+getClass()+"input: "+input);
         Optional<Product>productOptional=productRepository.findById(input);
 
         if(productOptional.isPresent()){
